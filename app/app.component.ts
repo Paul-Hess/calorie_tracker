9d69cf9bb159a0882ec2c17b19aa81a9ef4320a8
Paul-Hess <pauljacksonhess@gmail.com>
2016-03-24 18:51:22 -0700

    add initial files with first model

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1></h1>
+       
+  <div>
+    `
+})
+
+
+export class AppComponent {
+
+}
---
+++ app/meal.model.ts
@@ -0,0 +1,7 @@
+
+export class Meal {
+	public foodName: string;
+	public notes: string;
+	public calories: number;
+	constructor(food: string, notes: string, calories: number) {}
+}
